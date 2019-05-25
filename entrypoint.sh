4d98a0d7a415e0d0405207ebef55c7498ba09ab5
Andreas Möller <am@localheinz.com>
2019-05-25 10:40:58 +0200

    Fix: Remove remaining debug output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/sh -l
 
 sh -c "if [[ '$HOME' != '/root' ]]; then cp -r /root/.composer $HOME/.composer; fi"
-sh -c "/usr/local/bin/composer global show --direct"
-sh -c "/usr/local/bin/composer list"
 sh -c "/usr/local/bin/composer normalize --dry-run"
